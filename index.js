5f03005963b32ac2217649488ff818e45b8e4887
Michael Stallings <stall84@gmail.com>
2020-03-07 12:37:50 -0500

    made some styling changes (text-align-centered the movie divs). Also finished Part 1 by completing Step 3 with the search-bar eventListener

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ document.addEventListener('DOMContentLoaded', function() {
             <div class="movie card m-2" style="width: 15rem;">
                 <img class="card-img-top" src="${currentMovie.Poster}">
                     <div class="card-body">
-                        <h2 class="card-title">${currentMovie.Title}</h2>
-                        <h4 class="release-date">${currentMovie.Year}</h4>
+                        <h3 class="card-title">${currentMovie.Title}</h3>
+                        <h6 class="release-date">${currentMovie.Year}</h6>
                         <button class="btn btn-primary" name="addButton">Add</button>
                     </div>
             </div>
@@ -21,6 +21,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
         return movieHTML;
     }
-    var movieContainer = document.getElementById('movies-container');
-    movieContainer.innerHTML = renderMovies(movieData);
+
+        document.getElementById('myForm').addEventListener('submit', function(e) {
+            e.preventDefault()
+            var movieContainer = document.getElementById('movies-container');
+            movieContainer.innerHTML = renderMovies(movieData);
+        })
+    
 })
